7a9b03f46ca9986afd23d1c6f3aefb5774afb9e5
shonder <shonder@gmail.com>
2024-03-05 09:30:48 -0800

    quit margin in mobile resolution

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -54,9 +54,9 @@ export default function NavBar ({ lang, dictionary }:{lang: Locale, dictionary:a
   }, []);
 
   return (
-    <header className='fixed top-0 z-50  flex h-[65px] w-full   items-center justify-between  bg-[#03001417] py-1 shadow-lg shadow-[#2A0E61]/50 sm:flex '>
+    <header className='fixed top-0 z-50  flex h-[65px] w-full   items-center  justify-between  bg-[#03001417] py-1 shadow-lg shadow-[#2A0E61]/50 sm:flex '>
       <p className='ml-2 hidden w-full  font-mono text-[25px] text-white   sm:block'>{'<Leyner />'} </p>
-      <nav className={`ml-10 w-[300px] min-w-[300px] rounded-full border border-[#7042f861] bg-[#030014Se] lg:min-w-[700px] ${isVisible ? 'bg-[#2A0E61]/90' : ''} `}>
+      <nav className={`ml-3 w-[300px] min-w-[300px] rounded-full border border-[#7042f861] bg-[#030014Se] md:ml-10 lg:min-w-[700px] ${isVisible ? 'bg-[#2A0E61]/90' : ''} `}>
         <div className='flex justify-between  px-4 py-3 font-mono text-white '>
           {NavLinks.map((linkValue, item) => (
             <div key={linkValue.link} className='flex  hover:scale-110 hover:underline'>
